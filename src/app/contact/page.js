dc6665fd788684f4cff4bb8b31ae016157f35a25
mahfuj <mahfujahmad424@gmail.com>
2024-09-18 15:44:08 +0600

    initial commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+    return (
+        <div>
+            this is contact us page
+        </div>
+    );
+};
+
+export default ContactPage;
---
+++ src/app/news/article/page.js
@@ -0,0 +1,5 @@
+const ArticlePage = () => {
+  return <div>this is article page</div>;
+};
+
+export default ArticlePage;
